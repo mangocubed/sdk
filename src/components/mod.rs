cf8928ed15cb8d8482a3ff815017d7dc4bbd1475
Javier E. <jaedns@gmail.com>
2025-09-27 23:57:58 -0300

    Add component PageTitle (#1)

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,4 @@
+use dioxus::core::{DynamicNode, Template, TemplateNode};
 use dioxus::prelude::*;
 
 #[component]
@@ -87,5 +88,48 @@ pub fn NavbarEnd(children: Element) -> Element {
 pub fn NavbarStart(children: Element) -> Element {
     rsx! {
         div { class: "navbar-start", {children} }
+    }
+}
+
+#[component]
+pub fn PageTitle(children: Element) -> Element {
+    #[cfg(not(feature = "fullstack"))]
+    let mut app_title = "Mango³".to_owned();
+    #[cfg(feature = "fullstack")]
+    let mut app_title = use_server_cached(|| dioxus::cli_config::app_title().unwrap_or("Mango³".to_owned()));
+
+    if cfg!(debug_assertions) {
+        app_title += " (dev)";
+    }
+
+    let vnode = children?;
+    let page_title = match vnode.template {
+        Template {
+            roots: &[TemplateNode::Text { text }],
+            node_paths: &[],
+            attr_paths: &[],
+            ..
+        } => text.to_string(),
+        Template {
+            roots: &[TemplateNode::Dynamic { id }],
+            node_paths: &[&[0]],
+            attr_paths: &[],
+            ..
+        } => {
+            let node = &vnode.dynamic_nodes[id];
+            match node {
+                DynamicNode::Text(text) => text.value.clone(),
+                _ => {
+                    return rsx!();
+                }
+            }
+        }
+        _ => {
+            return rsx!();
+        }
+    };
+
+    rsx! {
+        document::Title { "{page_title} | {app_title}" }
     }
 }
