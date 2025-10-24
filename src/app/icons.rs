c108506e7e9419c17da539b62032a113849d3a4d
Javier E. <jaedns@gmail.com>
2025-10-24 12:08:00 -0300

    Add magnifying glass icon (#39)

--- src/app/icons.rs
+++ src/app/icons.rs
@@ -73,6 +73,23 @@ pub fn InformationCircleOutline() -> Element {
 }
 
 #[component]
+pub fn MagnifyingGlassMini() -> Element {
+    rsx! {
+        svg {
+            class: "size-5",
+            fill: "currentColor",
+            view_box: "0 0 20 20",
+            xmlns: "http://www.w3.org/2000/svg",
+            path {
+                clip_rule: "evenodd",
+                d: "M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z",
+                fill_rule: "evenodd",
+            }
+        }
+    }
+}
+
+#[component]
 pub fn Mango3Icon(#[props(optional)] class: String) -> Element {
     rsx! {
         svg {
