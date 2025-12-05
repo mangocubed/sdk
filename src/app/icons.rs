cab46fc5017585d403e83a2ce5faad366d96d960
Javier E. <jaedns@gmail.com>
2025-12-05 18:18:06 -0300

    Add BackspaceMini icon component (#77)

--- src/app/icons.rs
+++ src/app/icons.rs
@@ -1,6 +1,23 @@
 use dioxus::prelude::*;
 
 #[component]
+pub fn BackspaceMini() -> Element {
+    rsx! {
+        svg {
+            class: "size-5",
+            fill: "currentColor",
+            view_box: "0 0 20 20",
+            xmlns: "http://www.w3.org/2000/svg",
+            path {
+                clip_rule: "evenodd",
+                d: "M7.22 3.22A.75.75 0 0 1 7.75 3h9A2.25 2.25 0 0 1 19 5.25v9.5A2.25 2.25 0 0 1 16.75 17h-9a.75.75 0 0 1-.53-.22L.97 10.53a.75.75 0 0 1 0-1.06l6.25-6.25Zm3.06 4a.75.75 0 1 0-1.06 1.06L10.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L12 8.94l-1.72-1.72Z",
+                fill_rule: "evenodd",
+            }
+        }
+    }
+}
+
+#[component]
 pub fn CheckMini() -> Element {
     rsx! {
         svg {
