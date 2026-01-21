2306f18f563f9a1f65f82ddf0ba8748a4e8e58d1
Javier E. <jaedns@gmail.com>
2026-01-21 13:39:44 -0300

    Add DocumentTextSolid icon component (#97)

--- src/app/icons/mod.rs
+++ src/app/icons/mod.rs
@@ -2,22 +2,11 @@ use dioxus::prelude::*;
 
 mod mini;
 mod outline;
+mod solid;
 
 pub use mini::*;
 pub use outline::*;
-
-#[component]
-pub fn FolderSolid(#[props(default = "size-6".to_owned())] class: String) -> Element {
-    rsx! {
-        svg {
-            class,
-            fill: "currentColor",
-            view_box: "0 0 24 24",
-            xmlns: "http://www.w3.org/2000/svg",
-            path { d: "M19.5 21a3 3 0 0 0 3-3v-4.5a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3V18a3 3 0 0 0 3 3h15ZM1.5 10.146V6a3 3 0 0 1 3-3h5.379a2.25 2.25 0 0 1 1.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 0 1 3 3v1.146A4.483 4.483 0 0 0 19.5 9h-15a4.483 4.483 0 0 0-3 1.146Z" }
-        }
-    }
-}
+pub use solid::*;
 
 #[component]
 pub fn Mango3Icon(#[props(optional)] class: String) -> Element {
